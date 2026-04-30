9ac4d72d845824b17a4cf1af622e022db0bc5875
policeregion551-wq <policeregion551@gmail.com>
2026-04-30 07:33:36 -0700

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,17 +2,9 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCCJb4b-M59rxx2uo0Sxs3ynUVQorr50EI",
-  authDomain: "anti-phishing-security.firebaseapp.com",
-  projectId: "anti-phishing-security",
-  storageBucket: "anti-phishing-security.firebasestorage.app",
-  messagingSenderId: "148078331123",
-  appId: "1:148078331123:web:bfbdca4f0746735f6a6386",
-  measurementId: "G-BX0RD0DNX0"
-};
+import firebaseConfig from "../../firebase-applet-config.json";
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
 export default app;
